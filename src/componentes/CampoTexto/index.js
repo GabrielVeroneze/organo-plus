971a7d0655b0118f817f2a9ec449898fa70d0dc6
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-05-08 23:45:33 -0300

    :sparkles: feat: Captura o valor inserido no input

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,10 +1,18 @@
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
+
+    let valor = ''
+
+    const aoDigitado = (evento) => {
+        valor = evento.target.value
+        console.log(valor)
+    }
+
     return (
         <div className="campo-texto">
             <label className='campo-texto__label'>{props.label}</label>
-            <input required={props.obrigatorio} className='campo-texto__input' type="text" placeholder={props.placeholder} />
+            <input onChange={aoDigitado} required={props.obrigatorio} className='campo-texto__input' type="text" placeholder={props.placeholder} />
         </div>
     )
 }
