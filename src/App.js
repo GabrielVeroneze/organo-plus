7aa861ff5befe6c7fd0208d884508da31b01bec0
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-05-16 23:46:32 -0300

    :sparkles: feat: Eleva a lista de times

--- src/App.js
+++ src/App.js
@@ -6,6 +6,44 @@ import Time from './componentes/Time';
 // O componente principal que importa e renderiza os outros componentes
 function App() {
 
+    const times = [
+        {
+            nome: 'Programação',
+            corPrimaria: '#57C278',
+            corSecundaria: '#D9F7E9'
+        },
+        {
+            nome: 'Front-End',
+            corPrimaria: '#82CFFA',
+            corSecundaria: '#E8F8FF'
+        },
+        {
+            nome: 'Data Science',
+            corPrimaria: '#A6D157',
+            corSecundaria: '#F0F8E2'
+        },
+        {
+            nome: 'Devops',
+            corPrimaria: '#E06B69',
+            corSecundaria: '#FDE7E8'
+        },
+        {
+            nome: 'UX e Design',
+            corPrimaria: '#DB6EBF',
+            corSecundaria: '#FAE9F5'
+        },
+        {
+            nome: 'Mobile',
+            corPrimaria: '#FFBA05',
+            corSecundaria: '#FFF5D9'
+        },
+        {
+            nome: 'Inovação e Gestão',
+            corPrimaria: '#FF8A29',
+            corSecundaria: '#FFEEDF'
+        }
+    ]
+
     const [colaboradores, setColaboradores] = useState([]) 
 
     // Recebe os dados do novo colaborador que foram inseridos no formulário.
@@ -22,9 +60,7 @@ function App() {
                 // Passa uma função como propriedade para o componente 'Formulario', chamada 'aoColaboradorCadastrado', que é usada para atualizar o estado de colaboradores
                 aoColaboradorCadastrado={colaboradores => aoNovoColaboradorCadastrado(colaboradores)}
             />
-            <Time nome="Programação" />
-            <Time nome="Front-End" />
-            <Time nome="Data Science" />
+            {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria} />)}
         </div>
   );
 }
