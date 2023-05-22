60689153103893f107dacd9ee6ab03e748527faf
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-05-22 18:12:55 -0300

    :bulb: docs: Comentários sobre CampoTexto e ListaSuspensa

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -4,13 +4,23 @@ import './CampoTexto.css'
 const CampoTexto = (props) => {
 
     const aoDigitado = (evento) => {
+        // Chama a função 'aoAlterado' e passa como parâmetro o novo valor, a função é usada para atualizar o estado do componente pai com o novo valor
         props.aoAlterado(evento.target.value)
     }
 
     return (
         <div className="campo-texto">
             <label className='campo-texto__label'>{props.label}</label>
-            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} className='campo-texto__input' type="text" placeholder={props.placeholder} />
+            <input 
+                // Define o valor inicial do campo de texto, com base no valor passado através da propriedade 'valor' do componente 'CampoTexto'
+                value={props.valor}
+                // Adiciona o evento 'onChange' no input, portanto, quando o valor do campo de texto for alterado pelo usuário, a função 'aoDigitado' será executada
+                onChange={aoDigitado}
+                required={props.obrigatorio}
+                className='campo-texto__input'
+                type="text"
+                placeholder={props.placeholder}
+            />
         </div>
     )
 }
--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -3,14 +3,25 @@ import './ListaSuspensa.css'
 const ListaSuspensa = (props) => {
 
     const aoSelecionado = (evento) => {
+        // Chama a função 'aoAlterado' e passa como parâmetro o novo valor, a função é usada para atualizar o estado do componente pai com o novo valor
         props.aoAlterado(evento.target.value)
     }
 
     return (
-        <div className='lista'>
-            <label className='lista__label'>{props.label}</label>
-            <select value={props.valor} onChange={aoSelecionado} className='lista__menu' required={props.obrigatorio}>
-                {props.itens.map(item => <option key={item}>{item}</option>)}
+        <div className="lista">
+            <label className="lista__label">{props.label}</label>
+            <select
+                // Define o valor inicial da lista suspensa, com base no valor passado através da propriedade 'valor' do componente 'ListaSuspensa'
+                value={props.valor}
+                // Adiciona o evento 'onChange' no select, portanto, quando um item da lista for selecionado ou alterado pelo usuário, a função 'aoSelecionado' será executada
+                onChange={aoSelecionado}
+                className="lista__menu"
+                required={props.obrigatorio}
+            >
+                {props.itens.map(item => (
+                    // Cria os elementos <option> dentro do <select>, com base nos itens passados através da propriedade 'itens' do componente 'ListaSuspensa'
+                    <option key={item}>{item}</option>
+                ))}
             </select>
         </div>
     )
