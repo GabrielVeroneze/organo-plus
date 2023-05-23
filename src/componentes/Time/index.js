b5ad67618372bec128dbccf5aeaa9b43331ca3d8
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-05-22 23:45:05 -0300

    :sparkles: feat: Esconde times vazios

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -4,20 +4,22 @@ import './Time.css'
 const Time = (props) => {
 
     return (
-        <section className='time' style={{backgroundColor: props.corSecundaria}}>
-            <h3 className='time__titulo'>{props.nome}</h3>
-            <div className='time__sublinhado' style={{backgroundColor: props.corPrimaria}}></div>
-            <div className='time__colaboradores'>
-                {props.colaboradores.map(colaborador => (
-                    <Colaborador
-                        key={colaborador.nome}
-                        nome={colaborador.nome}
-                        cargo={colaborador.cargo}
-                        imagem={colaborador.imagem}
-                    />
-                ))}
-            </div>
-        </section>
+        props.colaboradores.length > 0 && (
+            <section className='time' style={{backgroundColor: props.corSecundaria}}>
+                <h3 className='time__titulo'>{props.nome}</h3>
+                <div className='time__sublinhado' style={{backgroundColor: props.corPrimaria}}></div>
+                <div className='time__colaboradores'>
+                    {props.colaboradores.map(colaborador => (
+                        <Colaborador
+                            key={colaborador.nome}
+                            nome={colaborador.nome}
+                            cargo={colaborador.cargo}
+                            imagem={colaborador.imagem}
+                        />
+                    ))}
+                </div>
+            </section>
+        ) 
     )
 }
 
