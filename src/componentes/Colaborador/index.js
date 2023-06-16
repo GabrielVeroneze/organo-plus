d356eeef8e2644986168e2780948a835790e49de
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-06-15 23:32:23 -0300

    :sparkles: feat: Adiciona funcionalidade de favoritar colaborador

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,21 +1,38 @@
 import './Colaborador.css'
 // Importa o ícone 'AiFillCloseCircle' do pacote de ícones 'react-icons'
-import { AiFillCloseCircle } from 'react-icons/ai'
+import { AiFillCloseCircle, AiFillHeart, AiOutlineHeart } from 'react-icons/ai'
 
-const Colaborador = ({nome, cargo, imagem, corDeFundo, aoDeletar, id}) => {
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar, aoFavoritar }) => {
+    
+    const handleClick = () => {
+        aoFavoritar(colaborador.id)
+    }
+
+    const propsFavorito = {
+        size: 25,
+        onClick: handleClick
+    }
+
     return (
         <div className='colaborador'>
             <AiFillCloseCircle
                 className='colaborador__deletar'
                 // Atrubui a função 'aoDeletar' ao evento de clique do componente, então, quando o ícone for clicado, a função 'aoDeletar' será executada passando o id do colaborador como argumento
-                onClick={() => aoDeletar(id)}
+                onClick={() => aoDeletar(colaborador.id)}
             />
             <div className='colaborador__cabecalho' style={{backgroundColor: corDeFundo}}>
-                <img className='colaborador__imagem' src={imagem} alt={nome} />
+                <img className='colaborador__imagem' src={colaborador.imagem} alt={colaborador.nome} />
             </div>
             <div className='colaborador__rodape'>
-                <h4 className='colaborador__nome'>{nome}</h4>
-                <h5 className='colaborador__cargo'>{cargo}</h5>
+                <h4 className='colaborador__nome'>{colaborador.nome}</h4>
+                <h5 className='colaborador__cargo'>{colaborador.cargo}</h5>
+                <div className='colaborador__favoritar'>
+                    {colaborador.favorito ? (
+                        <AiFillHeart {...propsFavorito} color='#FF0000' />
+                    ) : (
+                        <AiOutlineHeart {...propsFavorito} />
+                    )}
+                </div>
             </div>
         </div>
     )
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -3,7 +3,7 @@ import './Time.css'
 // Importa a biblioteca 'hex-to-rgba', ela fornece a função 'hexToRgba' que é utilizada para converter valores hexadecimais em valores rgba
 import hexToRgba from 'hex-to-rgba'
 
-const Time = ({time, colaboradores, aoDeletar, mudarCor}) => {
+const Time = ({time, colaboradores, aoDeletar, mudarCor, aoFavoritar}) => {
 
     const handleChange = (evento) => {
         // É uma função que foi passada como props para o componente 'Time', o 1º argumento é o valor da cor (evento.target.value) e o 2º argumento é o id do time (time.id)
@@ -27,17 +27,15 @@ const Time = ({time, colaboradores, aoDeletar, mudarCor}) => {
                 <h3 className='time__titulo'>{time.nome}</h3>
                 <div className='time__sublinhado' style={{backgroundColor: time.cor}}></div>
                 <div className='time__colaboradores'>
-                    {colaboradores.map((colaborador, indice) => {
+                    {colaboradores.map(colaborador => {
                         return (
                             <Colaborador
-                                key={indice}
-                                nome={colaborador.nome}
-                                cargo={colaborador.cargo}
-                                imagem={colaborador.imagem}
+                                key={colaborador.id}
+                                colaborador={colaborador}
                                 corDeFundo={time.cor}
                                 // A propriedade é usada para passar a referência da função 'aoDeletar' para cada instância do componente 'Colaborador'
                                 aoDeletar={aoDeletar}
-                                id={colaborador.id}
+                                aoFavoritar={aoFavoritar}
                             />
                         ) 
                     })}
